10fbec58712c01105f864f32cf7d0075e2f47408
RithvikKolluru <rithwikkolluru7@gmail.com>
2026-08-25 10:19:27 +0530

    refactor(frontend): update routing to redirect register to login and clean up imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
 import StudentDashboard from './pages/StudentDashboard';
 import ReportGarbage from './pages/ReportGarbage';
 import CoordinatorDashboard from './pages/CoordinatorDashboard';
@@ -30,7 +29,7 @@ function App() {
           <Routes>
             <Route path="/"         element={<Navigate to="/login" replace />} />
             <Route path="/login"    element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/register" element={<Navigate to="/login" replace />} />
 
             <Route path="/student" element={
               <PrivateRoute roles={['student']}>
